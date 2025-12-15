d41e77a821755bfc010d5361b29c911574abad4f
Muhammad Nauman <nauman33183@gmail.com>
2025-12-15 11:55:18 +0500

    nativewind fix

--- App.tsx
+++ App.tsx
@@ -1,10 +1,10 @@
-import "./globals.css"
+import "./global.css"
 import { View, Text } from 'react-native'
 import React from 'react'
 
 const App = () => {
   return (
-    <View className='flex-1 bg-red-500'>
+    <View className='flex-1 bg-red-500 items-center justify-center'>
       <Text className='text-white'>App</Text>
     </View>
   )
