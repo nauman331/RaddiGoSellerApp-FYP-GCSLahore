dbfb26ff49dc0eeceda18c8967fe48efe6a18112
Muhammad Nauman <nauman33183@gmail.com>
2025-12-27 18:56:35 +0500

    app name to eaddigo

--- android/app/src/main/java/com/raddigo/MainActivity.kt
+++ android/app/src/main/java/com/raddigo/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.trustfarokhtapp
+package com.raddigo
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- android/app/src/main/java/com/raddigo/MainApplication.kt
+++ android/app/src/main/java/com/raddigo/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.trustfarokhtapp
+package com.raddigo
 
 import android.app.Application
 import com.facebook.react.PackageList
